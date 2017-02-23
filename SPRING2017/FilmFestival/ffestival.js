059fe236b00f565536d5b5a044e0a89326d16356
parkb130 <brendonpark96@gmail.com>
2017-02-23 18:22:26 -0500

    asdf

--- SPRING2017/FilmFestival/ffestival.js
+++ SPRING2017/FilmFestival/ffestival.js
@@ -2,7 +2,7 @@ var Expand = (function() {
   var tile = $('.strips__strip');
   var tileLink = $('.strips__strip > .strip__content');
   var tileText = tileLink.find('.strip__inner-text');
-  var stripClose = ($'.strip__close');
+  var stripClose = $('.strip__close');
 
   var expanded = false;
 
@@ -12,7 +12,6 @@ var Expand = (function() {
 
     if (!expanded) {
       tile.addClass('strips__strip--expanded');
-      // add delay to inner text
       tileText.css('transition', 'all .5s .3s cubic-bezier(0.23, 1, 0.32, 1)');
       stripClose.addClass('strip__close--show');
       stripClose.css('transition', 'all .6s 1s cubic-bezier(0.23, 1, 0.32, 1)');
@@ -23,7 +22,6 @@ var Expand = (function() {
   var close = function() {
     if (expanded) {
       tile.removeClass('strips__strip--expanded');
-      // remove delay from inner text
       tileText.css('transition', 'all 0.15s 0 cubic-bezier(0.23, 1, 0.32, 1)');
       stripClose.removeClass('strip__close--show');
       stripClose.css('transition', 'all 0.2s 0s cubic-bezier(0.23, 1, 0.32, 1)')
