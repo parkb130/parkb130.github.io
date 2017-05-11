c0e098eb703b00cb01b0bd5481260c8c0a131675
parkb130 <brendonpark96@gmail.com>
2017-05-11 16:09:20 -0400

    asdf

---
+++ SPRING2017/Final/data.js
@@ -0,0 +1,67 @@
+var alltuts = [
+	{
+		"Title": "Photoshop Tutorial #1",
+		"Program": "Photoshop",
+		"Duration": 120,
+		"Subcategories": [
+				"Typography",
+				"Composition/Manipulation",
+				"Logo"
+		],
+		"Emotions": {
+			"Rating": 4.2,
+			"RageLevel": 9,
+			"Laziness": 2.3
+		}
+	},
+
+
+	{
+		"Title": "Illustrator Tutorial #1",
+		"Program": "Illustrator",
+		"Duration": 74,
+		"Subcategories": [
+				"Typography",
+				"Illustration",
+				"Logo"
+				],
+		"Emotions": {
+			"Rating": 4.2,
+			"RageLevel": 9,
+			"Laziness": 2.3
+		}
+	},	
+
+	{
+		"Title": "After Effects Tutorial #1",
+		"Program": "After Effects",
+		"Duration": 150,
+		"Subcategories": [
+				"Typography",
+				"Illustration",
+				"3D"
+				],
+		"Emotions": {
+			"Rating": 4.2,
+			"RageLevel": 9,
+			"Laziness": 2.3
+		}
+	},
+
+	{
+		"Title": "Cinema4D Tutorial #1",
+		"Program": "Cinema 4D",
+		"Duration": 250,
+		"Subcategories": [
+				"Typography",
+				"Displacement",
+				],
+		"Emotions": {
+			"Rating": 4.2,
+			"RageLevel": 9,
+			"Laziness": 2.3
+		}
+	}
+
+
+];
